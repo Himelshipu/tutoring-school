5e2e9867f2f82a502469a7645a65273eab7f40b4
furiousnur <furiousnur@email.com>
2021-06-25 22:57:03 +0600

    login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -67,6 +67,7 @@ class LoginController extends Controller
         $this->validate($request, $rules);
 
         $user = User::where('username', $request->get('username'))->orWhere('email', $request->get('username'))->first();
+ 
 //        $user = User::where('username', $request->get('username'))->first();
 
         $validCredentials = Hash::check($request['password'], $user->getAuthPassword());
